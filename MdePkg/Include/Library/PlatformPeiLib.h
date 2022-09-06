e3ef6c8b76e7211ffa6d55ddeb7aeb7d206af303
Sunil V L <sunilvl@ventanamicro.com>
2022-09-06 19:45:24 +0530

    MdePkg: Add PlatformPeiLib library for RISC-V
    This library is required in RISC-V to build the FDT Hob. The
    library can be leveraged by other architectures like ARM if
    required.
    
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Liming Gao <gaoliming@byosoft.com.cn>
    Cc: Zhiguang Liu <zhiguang.liu@intel.com>
    Cc: Daniel Schaefer <git@danielschaefer.me>
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>

---
+++ MdePkg/Include/Library/PlatformPeiLib.h
@@ -0,0 +1,15 @@
+/** @file
+  Library to initialize platform data at PEI phase
+
+  Copyright (c) 2021-2022, Hewlett Packard Development LP. All rights reserved.<BR>
+  Copyright (c) 2022, Ventana Micro Systems Inc. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#ifndef _PLATFORM_PEI_LIB_H_
+#define _PLATFORM_PEI_LIB_H_
+
+EFI_STATUS PlatformPeim(VOID);
+
+#endif
